09f9a3e89049f66488404fefef5cb0b4b7a82754
shardul <shardul.aeer@somaiya.edu>
2021-04-12 19:56:43 +0530

    feat: Deploy contract to Aflajores testnet

--- truffle.js
+++ truffle.js
@@ -1,6 +1,21 @@
 require('babel-register')
 require('babel-polyfill')
+require('dotenv').config()
 const HDWalletProvider = require('truffle-hdwallet-provider')
+const ContractKit = require('@celo/contractkit')
+const Web3 = require('web3')
+
+function getCeloProvider(network) {
+  const web3 = new Web3(`https://celo-${network}--rpc.datahub.figment.io/apikey/${process.env.DATAHUB_API_KEY}/`)
+  const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
+
+  const client = ContractKit.newKitFromWeb3(web3)
+  client.addAccount(account.privateKey)
+  return client.connection.web3.currentProvider
+}
+
+const alfajoresProvider = getCeloProvider('alfajores')
+const celoMainnetProvider = getCeloProvider('mainnet')
 
 module.exports = {
   networks: {
@@ -19,10 +34,15 @@ module.exports = {
       network_id: '3',
       skipDryRun: true,
     },
+    alfajores: { // Not yet supported by The Graph
+      provider: alfajoresProvider,
+      network_id: '44787',
+      skipDryRun: true,
+    }
   },
   compilers: {
     solc: {
-      version: '0.4.25'    // Fetch exact version from solc-bin (default: truffle's version)
+      version: '0.4.25'
     }
   }
 }
